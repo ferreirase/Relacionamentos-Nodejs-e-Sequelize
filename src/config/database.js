acdb3cd708227a8ffcab673690d93da5b020fd71
Ferreirase <42661796+ferreirase@users.noreply.github.com>
2019-10-22 09:50:54 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,18 @@
 module.exports = { 
+  //dialeto do Banco
   dialect: 'postgres', 
+  // endereço do host
   host: 'localhost', 
-  username: 'postgres', 
-  password: 'docker',
+  //usuário do banco
+  username: '******',
+  //senha do usuário no banco de dados
+  password: '******', 
+  //nome do banco
   database: 'sqlnode', 
   define: {
     //aqui estou definindo q todo registro do DB terá um campo created_at e updated_at
     timestamps: true, 
-    //permitindo o armazenamento de palavras compostar separadas por underline(_). chama Snake Case
+    //permitindo o armazenamento de palavras compostar separadas por underline(_). estilo chamado Snake Case
     underscored: true
   }
 
